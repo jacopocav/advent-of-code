8db1d75d2649801ab9cf2ba23b4de26c2422a513
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-10 22:45:15 +0100

    Simplified Position adjacency check (day 09)

--- src/main/kotlin/ceejay/advent/day09/Position.kt
+++ src/main/kotlin/ceejay/advent/day09/Position.kt
@@ -11,14 +11,7 @@ data class Position(val row: Int, val column: Int) {
         val rowDistance = abs(row - other.row)
         val colDistance = abs(column - other.column)
 
-        // Overlapping
-        return this == other
-            // Vertically adjacent
-            || (row == other.row && colDistance == 1)
-            // Horizontally adjacent
-            || (column == other.column && rowDistance == 1)
-            // Diagonally adjacent
-            || (rowDistance == 1 && colDistance == 1)
+        return rowDistance <= 1 && colDistance <= 1
     }
 
     override fun toString(): String {
