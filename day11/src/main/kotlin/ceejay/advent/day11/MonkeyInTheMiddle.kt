f1daa4c4fa23e0e20964d3f0d5aca39b9b876499
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-11 19:48:24 +0100

    Created day 11 module

---
+++ day11/src/main/kotlin/ceejay/advent/day11/MonkeyInTheMiddle.kt
@@ -0,0 +1,15 @@
+package ceejay.advent.day11
+
+fun main() {
+    println("Part 1 Result: ${part1()}")
+    println("Part 2 Result: ${part2()}")
+}
+
+fun part1(): Int {
+    TODO()
+}
+
+fun part2(): Int {
+    TODO()
+}
+
---
+++ day11/src/test/kotlin/functional/Day10Test.kt
@@ -0,0 +1,27 @@
+package functional
+
+import ceejay.advent.day11.part1
+import ceejay.advent.day11.part2
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+object Day10Test {
+
+    @Test
+    fun part1Test() {
+        // when
+        val actual = part1()
+
+        // then
+        then(actual).isEqualTo(TODO())
+    }
+
+    @Test
+    fun part2Test() {
+        // when
+        val actual = part2()
+
+        // then
+        then(actual).isEqualTo(TODO())
+    }
+}
