c0a1c8dcad9cfb4a5cfc8d67a75c496cca7b9d5c
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-13 13:29:26 +0100

    Created day 13 module

---
+++ day13/src/test/kotlin/functional/Day13Test.kt
@@ -0,0 +1,27 @@
+package functional
+
+import ceejay.advent.day13.part1
+import ceejay.advent.day13.part2
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+object Day13Test {
+
+    @Test
+    fun part1Test() {
+        // when
+        val actual = part1()
+
+        // then
+        then(actual).isEqualTo(0) // TODO replace with correct value
+    }
+
+    @Test
+    fun part2Test() {
+        // when
+        val actual = part2()
+
+        // then
+        then(actual).isEqualTo(0) // TODO replace with correct value
+    }
+}
