d44597afa308da8b2ccd264736e739edc0343d72
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-14 08:52:08 +0100

    Created day 14 module

--- common/src/main/kotlin/ceejay/advent/util/ListExtensions.kt
+++ common/src/main/kotlin/ceejay/advent/util/ListExtensions.kt
@@ -9,9 +9,8 @@ inline fun <T> MutableList<T>.removeWhile(crossinline predicate: (T) -> Boolean)
 
 fun List<Char>.composeString(): String = joinToString("")
 
-fun <T> Sequence<T>.indicesOfAll(predicate: (T) -> Boolean): Sequence<Int> =
-    mapIndexed { i, elem -> Pair(i, elem) }
-        .filter { (_, elem) -> predicate(elem) }
-        .map { it.first }
+fun <T> Sequence<T>.indicesOfAll(predicate: (T) -> Boolean): Sequence<Int> = withIndex()
+    .filter { (_, elem) -> predicate(elem) }
+    .map { it.index }
 
 fun Sequence<Int>.product(): Int = fold(1) { a, b -> a * b }
