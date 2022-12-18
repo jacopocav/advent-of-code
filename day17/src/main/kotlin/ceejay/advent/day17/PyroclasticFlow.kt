5a34852b488355a31a6b9c7541c493bb548b9716
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-18 11:18:25 +0100

    Created day 17 module

---
+++ day17/src/main/kotlin/ceejay/advent/day17/PyroclasticFlow.kt
@@ -0,0 +1,16 @@
+package ceejay.advent.day17
+
+import ceejay.advent.util.InputFile
+
+fun main() {
+    println("Part 1 Result: ${part1()}")
+    println("Part 2 Result: ${part2()}")
+}
+
+fun part1(): Int = InputFile.withLines {
+    TODO()
+}
+
+fun part2(): Int = InputFile.withLines {
+    TODO()
+}
