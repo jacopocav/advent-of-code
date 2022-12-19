32e1d336818b32e258287e419bba1fabbf26953f
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-19 14:26:38 +0100

    Created day 18 module

---
+++ day18/src/main/kotlin/ceejay/advent/day18/BoilingBoulders.kt
@@ -0,0 +1,16 @@
+package ceejay.advent.day18
+
+import ceejay.advent.util.InputFile
+
+fun main() {
+    println("Part 1 Result: ${part1()}")
+    println("Part 2 Result: ${part2()}")
+}
+
+fun part1(): Long = InputFile.withLines {
+    TODO()
+}
+
+fun part2(): Long = InputFile.withLines {
+    TODO()
+}
