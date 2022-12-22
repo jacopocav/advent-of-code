0bd6efbb535bd4d8c96d87358cf4e81899826b2f
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-22 12:40:17 +0100

    Day 20 part 2 completed

--- day20/src/main/kotlin/ceejay/advent/day20/GrovePositioningSystem.kt
+++ day20/src/main/kotlin/ceejay/advent/day20/GrovePositioningSystem.kt
@@ -8,44 +8,65 @@ fun main() {
     part1().apply {
         println("Part 1 Result: $result (total time: $time)")
     }
-//    part2().apply {
-//        println("Part 2 Result: $result (total time: $time)")
-//    }
-}
-
-data class Number(val value: Int, var moved: Boolean)
-
-fun part1(): TimedResult<Any> = InputFile.withLines {
-    timed {
-        val numbers = map { it.toInt() }.map { Number(it, false) }.toMutableList()
-        val count = numbers.size
-//        println(numbers.map { it.value })
-
-        while (true) {
-            val i = numbers.indexOfFirst { !it.moved && it.value != 0 }
-                .takeIf { it >= 0 }
-                ?: break
-
-            val number = numbers[i]
-
-            numbers.removeAt(i)
-
-            val shift = (i + number.value) % (count - 1)
-
-            val newIndex = if (shift < 0) count - 1 + shift else shift
-
-            numbers.add(newIndex, number)
-            number.moved = true
-//            println(numbers.map { it.value })
-        }
-
-        val result = numbers.map { it.value }
-        val zero = result.indexOf(0)
-
-        result[(zero + 1000) % count] + result[(zero + 2000) % count] + result[(zero + 3000) % count]
+    part2().apply {
+        println("Part 2 Result: $result (total time: $time)")
     }
 }
 
-fun part2(): TimedResult<Any> = InputFile.withLines {
-    TODO()
+data class Number(val originalIndex: Int, val value: Long, var moved: Boolean)
+
+fun part1(): TimedResult<Long> = InputFile.withLines {
+    timed {
+        val numbers = toNumbers()
+        numbers.run()
+        numbers.getResult()
+    }
+}
+
+fun part2(): TimedResult<Long> = InputFile.withLines {
+    timed {
+        val numbers = toNumbers(distressSignal = 811589153)
+
+        repeat(10) {
+            numbers.run()
+            numbers.forEach { it.moved = false }
+        }
+
+        numbers.getResult()
+    }
+}
+
+private fun Sequence<String>.toNumbers(distressSignal: Long = 1) =
+    map { it.toLong() * distressSignal }
+        .mapIndexed { i, num -> Number(originalIndex = i, value = num, moved = false) }
+        .toMutableList()
+
+private fun MutableList<Number>.run() {
+    val count = size
+
+    while (true) {
+        val number = filter { !it.moved && it.value != 0L }
+            .minByOrNull { it.originalIndex }
+            ?: break
+
+        val i = indexOf(number)
+
+        removeAt(i)
+
+        val shift = (i + number.value) % (count - 1)
+
+        val newIndex = if (shift < 0) count - 1 + shift else shift
+
+        add(newIndex.toInt(), number)
+        number.moved = true
+    }
+}
+
+private fun List<Number>.getResult(): Long {
+    val result = map { it.value }
+    val zero = result.indexOf(0)
+
+    return result[(zero + 1000) % result.size] +
+        result[(zero + 2000) % result.size] +
+        result[(zero + 3000) % result.size]
 }
--- day20/src/test/kotlin/functional/Day20Test.kt
+++ day20/src/test/kotlin/functional/Day20Test.kt
@@ -3,7 +3,6 @@ package functional
 import ceejay.advent.day20.part1
 import ceejay.advent.day20.part2
 import org.assertj.core.api.BDDAssertions.then
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 object Day20Test {
@@ -18,12 +17,11 @@ object Day20Test {
     }
 
     @Test
-    @Disabled
     fun part2Test() {
         // when
         val actual = part2()
 
         // then
-        then(actual.result).isEqualTo(TODO())
+        then(actual.result).isEqualTo(5_382_459_262_696)
     }
 }
