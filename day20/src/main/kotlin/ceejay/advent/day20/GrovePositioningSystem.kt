8dad6dedadd4d87abc1af26dfb73e43917cb64c3
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-22 11:52:44 +0100

    Day 20 part 1 completed

--- day20/src/main/kotlin/ceejay/advent/day20/GrovePositioningSystem.kt
+++ day20/src/main/kotlin/ceejay/advent/day20/GrovePositioningSystem.kt
@@ -2,18 +2,48 @@ package ceejay.advent.day20
 
 import ceejay.advent.util.InputFile
 import ceejay.advent.util.TimedResult
+import ceejay.advent.util.timed
 
 fun main() {
     part1().apply {
         println("Part 1 Result: $result (total time: $time)")
     }
-    part2().apply {
-        println("Part 2 Result: $result (total time: $time)")
-    }
+//    part2().apply {
+//        println("Part 2 Result: $result (total time: $time)")
+//    }
 }
 
+data class Number(val value: Int, var moved: Boolean)
+
 fun part1(): TimedResult<Any> = InputFile.withLines {
-    TODO()
+    timed {
+        val numbers = map { it.toInt() }.map { Number(it, false) }.toMutableList()
+        val count = numbers.size
+//        println(numbers.map { it.value })
+
+        while (true) {
+            val i = numbers.indexOfFirst { !it.moved && it.value != 0 }
+                .takeIf { it >= 0 }
+                ?: break
+
+            val number = numbers[i]
+
+            numbers.removeAt(i)
+
+            val shift = (i + number.value) % (count - 1)
+
+            val newIndex = if (shift < 0) count - 1 + shift else shift
+
+            numbers.add(newIndex, number)
+            number.moved = true
+//            println(numbers.map { it.value })
+        }
+
+        val result = numbers.map { it.value }
+        val zero = result.indexOf(0)
+
+        result[(zero + 1000) % count] + result[(zero + 2000) % count] + result[(zero + 3000) % count]
+    }
 }
 
 fun part2(): TimedResult<Any> = InputFile.withLines {
--- day20/src/test/kotlin/functional/Day20Test.kt
+++ day20/src/test/kotlin/functional/Day20Test.kt
@@ -6,7 +6,6 @@ import org.assertj.core.api.BDDAssertions.then
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-@Disabled
 object Day20Test {
 
     @Test
@@ -15,10 +14,11 @@ object Day20Test {
         val actual = part1()
 
         // then
-        then(actual.result).isEqualTo(TODO())
+        then(actual.result).isEqualTo(872)
     }
 
     @Test
+    @Disabled
     fun part2Test() {
         // when
         val actual = part2()
