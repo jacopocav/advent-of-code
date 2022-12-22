9cae7ee07c0aedaccaffe419f5a0b6b010f73031
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-22 08:50:48 +0100

    Created day 20 module

---
+++ day20/src/main/kotlin/ceejay/advent/day20/GrovePositioningSystem.kt
@@ -0,0 +1,21 @@
+package ceejay.advent.day20
+
+import ceejay.advent.util.InputFile
+import ceejay.advent.util.TimedResult
+
+fun main() {
+    part1().apply {
+        println("Part 1 Result: $result (total time: $time)")
+    }
+    part2().apply {
+        println("Part 2 Result: $result (total time: $time)")
+    }
+}
+
+fun part1(): TimedResult<Any> = InputFile.withLines {
+    TODO()
+}
+
+fun part2(): TimedResult<Any> = InputFile.withLines {
+    TODO()
+}
