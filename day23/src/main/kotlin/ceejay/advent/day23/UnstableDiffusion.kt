cf61988ddfb2def7d1e8bb4dfb3fbe4d31013739
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-27 14:16:18 +0100

    Created day 23 module

---
+++ day23/src/main/kotlin/ceejay/advent/day23/UnstableDiffusion.kt
@@ -0,0 +1,24 @@
+package ceejay.advent.day23
+
+import ceejay.advent.util.InputFile
+import ceejay.advent.util.TimedResult
+import ceejay.advent.util.Vector2D.Companion.vector
+import ceejay.advent.util.removeWhile
+import ceejay.advent.util.timed
+
+fun main() {
+    part1().also {
+        println("Part 1 Result: $it")
+    }
+    part2().also {
+        println("Part 2 Result: $it")
+    }
+}
+
+fun part1(): TimedResult<Int> = InputFile.withLines {
+    timed { TODO() }
+}
+
+fun part2(): TimedResult<Int> = InputFile.withLines {
+    timed { TODO() }
+}
