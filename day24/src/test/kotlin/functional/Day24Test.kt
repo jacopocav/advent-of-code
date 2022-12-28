515cb4cd2b75a744d1d65766e24d7abe53479572
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-28 08:21:33 +0100

    Created day 24 module

---
+++ day24/src/test/kotlin/functional/Day24Test.kt
@@ -0,0 +1,29 @@
+package functional
+
+import ceejay.advent.day24.part1
+import ceejay.advent.day24.part2
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+@Disabled
+object Day24Test {
+
+    @Test
+    fun part1Test() {
+        // when
+        val actual = part1()
+
+        // then
+        then(actual.result).isEqualTo(TODO())
+    }
+
+    @Test
+    fun part2Test() {
+        // when
+        val actual = part2()
+
+        // then
+        then(actual.result).isEqualTo(TODO())
+    }
+}
