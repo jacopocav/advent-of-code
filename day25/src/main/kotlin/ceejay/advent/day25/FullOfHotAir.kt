cd7bbeebd83aa887b68895aa915e0040ac399359
Jacopo Cavallarin <jacopo.cavallarin@overit.it>
2022-12-29 09:03:48 +0100

    Created day 25 module

---
+++ day25/src/main/kotlin/ceejay/advent/day25/FullOfHotAir.kt
@@ -0,0 +1,26 @@
+package ceejay.advent.day25
+
+import ceejay.advent.util.InputFile
+import ceejay.advent.util.TimedResult
+import ceejay.advent.util.timed
+
+fun main() {
+    part1().also {
+        println("Part 1 Result: $it")
+    }
+    part2().also {
+        println("Part 2 Result: $it")
+    }
+}
+
+fun part1(): TimedResult<Int> = InputFile.withLines {
+    timed {
+        TODO()
+    }
+}
+
+fun part2(): TimedResult<Int> = InputFile.withLines {
+    timed {
+        TODO()
+    }
+}
